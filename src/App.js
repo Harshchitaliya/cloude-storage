2680971e71ffd4ba1ce6151574b0cfe7f2caf5fa
Harshchitaliya <chitaliyaharsh09@gmail.com>
2024-09-05 12:23:49 +0530

    add a home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import LoginForm from './components/login';
 import './App.css';
 import NavBar from './components/Navbar';
-import { useState } from 'react';
-
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './components/home';
 
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+  ])
 
   return (
 
     <>
-    <NavBar />
-    <LoginForm/>
+
+      <NavBar />
+      <LoginForm />
 
     </>
   );
---
+++ src/components/home.jsx
@@ -0,0 +1,6 @@
+function home(){
+ <h1> welcome to home</h1>
+};
+
+
+export default Home;
